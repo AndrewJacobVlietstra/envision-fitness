218966d11bf6b0f9dbeb8e2a6d741fe486b1a2ce
Andrew Vlietstra <46541660+AndrewJacobVlietstra@users.noreply.github.com>
2021-01-12 10:20:57 -0500

    Add files via upload
    Lots of work, added a ton of sections, fixed a couple weird bugs like when I would click a button to smooth scroll it would focus on the section and add a thin black border around it, so thats gone now. Also the fitness classes part was tricky, instead of using clip-path polygon, which what I was trying to do at first, I just photoshopped the photo to have the slice effect. That way I would still have the border to give it a clean look.

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,30 +5,16 @@ $(document).ready(function (){
 
     /* For sticky navigation */
 
-    $('.js--section-features').waypoint(function(direction) {
-        if (direction == "down") {
-            $('nav').addClass('sticky');
-        } else {
-            $('nav').removeClass('sticky');
-        }
-    }, {
-            offset: '50px;'
-    })
-
-    /* Alternate Method for Sticky Navigation */
-    // window.onscroll = function() {myFunction()};
-
-    // var navbar = document.getElementById("navBar");
-    // var sticky = stickyNavTriggerPoint.offsetTop;
-    // var offset = -90;
-
-    // function myFunction() {
-    //     if (window.pageYOffset >= (sticky + offset)) {
-    //     navbar.classList.add("sticky")
+    // $('.js--section-features').waypoint(function(direction) {
+    //     if (direction == "down") {
+    //         $('nav').addClass('sticky');
     //     } else {
-    //     navbar.classList.remove("sticky");
+    //         $('nav').removeClass('sticky');
     //     }
-    // }
+    // }, {
+    //         offset: '50px;'
+    // })
+
 
 
 
@@ -55,7 +41,7 @@ $(document).ready(function (){
             event.preventDefault();
             $('html, body').animate({
             scrollTop: target.offset().top
-            }, 1300, function() {
+            }, 1000, function() {
             // Callback after animation
             // Must change focus!
             var $target = $(target);
@@ -63,8 +49,6 @@ $(document).ready(function (){
             if ($target.is(":focus")) { // Checking if the target was focused
                 return false;
             } else {
-                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-                $target.focus(); // Set focus again
             };
             });
         }
@@ -75,73 +59,35 @@ $(document).ready(function (){
 
     /* Animations on Scroll */
 
-
-
-    /* Features */
-    $('.wp1').waypoint(function(direction) {
-        $('.wp1').addClass('animate__animated animate__fadeIn');
-    }, {
-        offset: '56%;'
-    });
-
-
-    /* Phone in Steps section */
-    $('.wp2').waypoint(function(direction) {
-        $('.wp2').addClass('animate__animated animate__fadeInUp');
-    }, {
-        offset: '50%;'
-    });
-
-
-    /* Cities section */
-    $('.wp3').waypoint(function(direction) {
-        $('.wp3').addClass('animate__animated animate__fadeIn');
-    }, {
-        offset: '55%;'
-    });
-
-    /* Sign-up Plans section */
-    $('.wp4').waypoint(function(direction) {
-        $('.wp4').addClass('animate__animated animate__fadeIn');
-    }, {
-        offset: '55%;'
-    });
-
-    /* Footer Links */
-    $('.wp5').waypoint(function(direction) {
-        $('.wp5').addClass('animate__animated animate__fadeInUp');
-    }, {
-        offset: '90%;'
-    });
-
-    /* Footer copyright statement */
-    $('.wp6').waypoint(function(direction) {
-        $('.wp6').addClass('animate__animated animate__fadeIn');
-    }, {
-        offset: '92%;'
-    });
+    // /* Features */
+    // $('.wp1').waypoint(function(direction) {
+    //     $('.wp1').addClass('animate__animated animate__fadeIn');
+    // }, {
+    //     offset: '56%;'
+    // });
 
 
 
-    /* Mobile Navigation */
-    $('.js--nav-icon').click(function() {
-        var nav = $('.js--main-nav');
-        var icon = $('.js--nav-icon i');
 
-        nav.slideToggle(400);
-        if (icon.hasClass('ion-navicon-round')) {
-            icon.removeClass('ion-navicon-round');
-            icon.addClass('ion-close-round');
+    // /* Mobile Navigation */
+    // $('.js--nav-icon').click(function() {
+    //     var nav = $('.js--main-nav');
+    //     var icon = $('.js--nav-icon i');
+
+    //     nav.slideToggle(400);
+    //     if (icon.hasClass('ion-navicon-round')) {
+    //         icon.removeClass('ion-navicon-round');
+    //         icon.addClass('ion-close-round');
             
-        } else {
-            icon.removeClass('ion-close-round');
-            icon.addClass('ion-navicon-round');
+    //     } else {
+    //         icon.removeClass('ion-close-round');
+    //         icon.addClass('ion-navicon-round');
             
 
-        }
+    //     }
 
 
-    })
+    // })
 
 
 });
