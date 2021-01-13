dd087367d866fc6c2a574ce2f88f9dd2a09beb93
Andrew Vlietstra <46541660+AndrewJacobVlietstra@users.noreply.github.com>
2021-01-12 19:54:31 -0500

    Add files via upload
    Finished PC layout of website, images are optimized, JS animations are implemented. All thats left to do is begin work on the queries to make it mobile/tablet compatible, and maybe also add a sticky nav? Im not sure yet.

--- resources/js/script.js
+++ resources/js/script.js
@@ -59,14 +59,75 @@ $(document).ready(function (){
 
     /* Animations on Scroll */
 
-    // /* Features */
-    // $('.wp1').waypoint(function(direction) {
-    //     $('.wp1').addClass('animate__animated animate__fadeIn');
-    // }, {
-    //     offset: '56%;'
-    // });
+    /* Why Work With Us */
+    $('.wp1').waypoint(function(direction) {
+        $('.wp1').addClass('animate__animated animate__fadeIn');
+    }, {
+        offset: '60%;'
+    });
 
+    /* Envision Testimonials */
+    $('.wp2').waypoint(function(direction) {
+        $('.wp2').addClass('animate__animated animate__fadeIn');
+    }, {
+        offset: '52%;'
+    });
 
+    /* Fitness Classes - Cardio */
+    $('.wp3').waypoint(function(direction) {
+        $('.wp3').addClass('animate__animated animate__fadeInLeft');
+    }, {
+        offset: '60%;'
+    });
+
+    /* Fitness Classes - Yoga */
+    $('.wp4').waypoint(function(direction) {
+        $('.wp4').addClass('animate__animated animate__fadeInRight');
+    }, {
+        offset: '50%;'
+    });
+
+    /* Fitness Classes - Strength */
+    $('.wp5').waypoint(function(direction) {
+        $('.wp5').addClass('animate__animated animate__fadeInLeft');
+    }, {
+        offset: '60%;'
+    });
+
+    /* Fitness Classes - Cycling */
+    $('.wp6').waypoint(function(direction) {
+        $('.wp6').addClass('animate__animated animate__fadeInRight');
+    }, {
+        offset: '50%;'
+    });
+
+    /* Sign Up Prices */
+    $('.wp7').waypoint(function(direction) {
+        $('.wp7').addClass('animate__animated animate__fadeIn');
+    }, {
+        offset: '50%;'
+    });
+
+    /* Footer Links */
+    $('.wp8').waypoint(function(direction) {
+        $('.wp8').addClass('animate__animated animate__fadeIn');
+    }, {
+        offset: '90%;'
+    });
+
+    /* Footer Icons */
+    $('.wp9').waypoint(function(direction) {
+        $('.wp9').addClass('animate__animated animate__fadeIn');
+    }, {
+        offset: '90%;'
+    });
+
+    /* Copyright Statement */
+    $('.wp10').waypoint(function(direction) {
+        $('.wp10').addClass('animate__animated animate__fadeIn');
+    }, {
+        offset: '90%;'
+    });
 
 
     // /* Mobile Navigation */
