1deed60398f8f78c7e127bf66788a57f556294bf
Andrew Vlietstra <46541660+AndrewJacobVlietstra@users.noreply.github.com>
2021-01-22 09:37:36 -0500

    Add files via upload
    Added sticky nav for desktop and mobile

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,15 +5,15 @@ $(document).ready(function (){
 
     /* For sticky navigation */
 
-    // $('.js--section-features').waypoint(function(direction) {
-    //     if (direction == "down") {
-    //         $('nav').addClass('sticky');
-    //     } else {
-    //         $('nav').removeClass('sticky');
-    //     }
-    // }, {
-    //         offset: '50px;'
-    // })
+    $('.red-banner-down-arrow').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+            offset: '50px;'
+    });
 
 
 
